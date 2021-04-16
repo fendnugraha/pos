29dace4fcc32944cf532160014ec4f66523bcfc8
fendnugraha <fendnugraha92@gmail.com>
2021-04-16 22:53:26 +0700

    redesign login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -6,53 +6,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GSM POS</title>
     <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>bootstrap.min.css">
+    <link rel="shortcut icon" href="<?= base_url('assets/img/'); ?>marker.png" type="image/x-icon">
+    <link rel="icon" href="<?= base_url('assets/img/'); ?>marker.png" type="image/x-icon">
+    <style>
+        body {
+            padding-top: 5%;
+        }
+
+        @media (max-width: 991.98px) {
+            .logo-login {
+                display: none;
+            }
+        }
+    </style>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">DNA Network Inc.</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <!-- <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li> -->
-                </ul>
-                <form class="d-flex" action="<?= base_url('auth'); ?>" method="post">
-                    <input type="text" class="form-control me-2" id="uname" name="uname" placeholder="Username">
-                    <input type="password" class="form-control me-2" id="password" name="password" placeholder="Password">
-                    <button class="btn btn-outline-primary me-2" type="submit">Login</button>
-                </form>
-                <a class="btn btn-outline-success" href="<?= base_url('auth/register'); ?>">Register</a>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md logo-login">
+                <img class="" src="<?= base_url('assets/img/'); ?>login-logo.png" alt="login-logo" width="100%">
+            </div>
+            <div class="col-md">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <h4 class="card-title text-center mb-3 mt-3">Account Login</h4>
+                        <form action="<?= base_url('auth'); ?>" method="post" class="mt-5">
+                            <div class="mb-3">
+                                <input type="text" class="form-control me-2" id="uname" name="uname" placeholder="Username">
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control me-2" id="password" name="password" placeholder="Password">
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-dark me-2" type="submit" style="width: 50%;">Login</button>
+                            </div>
+                        </form>
+                        <p>Tidak punya akun? Daftar <a href="<?= base_url('auth/register'); ?>">disini!</a></p>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
-    <div class="container">
-        <?= validation_errors('<span class="error text-danger">', '</span>'); ?>
-
-
     </div>
 
 </body>
