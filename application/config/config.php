655b302646f5cc322264cec722b4faef8b02f9a3
fendnugraha <fendnugraha@fendnugrahas-iMac.local>
2021-04-17 19:05:57 +0700

    fix config base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,10 @@ date_default_timezone_set('Asia/Bangkok');
 | a PHP script and you can easily do that on your own.
 |
 */
-$iplokal = $_SERVER['SERVER_ADDR'];
-$config['base_url'] = 'http://' . $iplokal . '/pos/';
+// $config['base_url'] = 'http://localhost/pos/';
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
